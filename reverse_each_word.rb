89d73ff050fd5b8f03e253939ac4e74bc0f65fcd
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-15 16:13:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
- now_array = string.split("")
+ now_array = string.split(" ")
  later_array =[]
  now_array.each do |string|
    later_array << string.reverse 
   end 
-  later_array.join("")
+  later_array.join(" ")
 end
